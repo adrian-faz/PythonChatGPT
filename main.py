488ac3327b75055662a655c817fcccfdb2a59c0a
Adrián Faz <75438122+adrian-faz@users.noreply.github.com>
2023-05-04 12:56:11 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -25,5 +25,6 @@ def getChatResponse():
     return {"response": model_response}
 
 
+
 if __name__ == "__main__":
-    app.run(debug=True) #Con el True en debug se reinicia cuando hay cambios
+    app.run(host='0.0.0.0', port='5000', debug=True) #Con el True en debug se reinicia cuando hay cambios
