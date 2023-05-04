79f94ea2c79116fba0f2dc308f925535922f9ba7
Adrián Faz <75438122+adrian-faz@users.noreply.github.com>
2023-05-04 10:52:08 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from dotenv import load_dotenv, find_dotenv
+from flask_cors import CORS
+import openai
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+def get_chatgpt_response(messages):
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=messages
+    )
+
+    return response['choices'][0]['message']['content']
+
+@app.route('/chatgpt/chat', methods=['POST'])
+def getChatResponse():
+    messages = request.json['messages']
+    print(messages)
+    load_dotenv()
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    model_response = get_chatgpt_response(messages)
+    return {"response": model_response}
+
+
+if __name__ == "__main__":
+    app.run(debug=True) #Con el True en debug se reinicia cuando hay cambios
